1a66d24fe6918421526dbee4ef8954bd64cf471d
Abdush <abdush.shaikh@gmail.com>
2022-02-20 21:37:19 +0530

    Dev Change

--- lib/view/screens/prescription/prescription.dart
+++ lib/view/screens/prescription/prescription.dart
@@ -3,8 +3,10 @@ import 'package:flutter_grocery/data/model/response/product_model.dart';
 import 'package:flutter_grocery/helper/responsive_helper.dart';
 import 'package:flutter_grocery/provider/cart_provider.dart';
 import 'package:flutter_grocery/provider/splash_provider.dart';
+import 'package:flutter_grocery/utill/color_resources.dart';
 import 'package:flutter_grocery/view/base/main_app_bar.dart';
 import 'package:flutter_grocery/view/screens/product/widget/details_app_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class PrescriptionScreen extends StatelessWidget {
@@ -12,18 +14,35 @@ class PrescriptionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Provider.of<SplashProvider>(context, listen: false).initSharedData();
     Provider.of<CartProvider>(context, listen: false).getCartData();
 
     Variations _variation;
     final GlobalKey<DetailsAppBarState> _key = GlobalKey();
 
     return Scaffold(
-      backgroundColor: Theme.of(context).cardColor,
-      appBar: ResponsiveHelper.isDesktop(context)
-          ? MainAppBar()
-          : DetailsAppBar(key: _key),
-      body: Text("hello"),
-    );
+        backgroundColor: ColorResources.getBackgroundColor(context),
+        appBar: ResponsiveHelper.isDesktop(context)
+            ? MainAppBar()
+            : DetailsAppBar(key: _key),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "Upload",
+                    style: TextStyle(color: Colors.grey.shade400),
+                  ),
+                ],
+              ),
+              Center(
+                child: Container(
+                  child: Image.asset("assets/image/valid.png"),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
