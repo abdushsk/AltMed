257b54801722aa89e92e9b3346e01378947f152c
Alshan Shaikh <faizshaikh.dev@gmail.com>
2022-02-20 15:47:12 +0530

    State Management Complete

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_grocery/helper/product_type.dart';
 import 'package:flutter_grocery/helper/responsive_helper.dart';
@@ -16,20 +18,33 @@ import 'package:flutter_grocery/view/screens/home/widget/product_view.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
-
-
   Future<void> _loadData(BuildContext context, bool reload) async {
     // await Provider.of<CategoryProvider>(context, listen: false).getCategoryList(context, reload);
 
     await Provider.of<CategoryProvider>(context, listen: false).getCategoryList(
-      context, Provider.of<LocalizationProvider>(context, listen: false).locale.languageCode,reload,
+      context,
+      Provider.of<LocalizationProvider>(context, listen: false)
+          .locale
+          .languageCode,
+      reload,
     );
-    await Provider.of<BannerProvider>(context, listen: false).getBannerList(context, reload);
-    await Provider.of<ProductProvider>(context, listen: false).getDailyItemList(context, reload,
-      Provider.of<LocalizationProvider>(context, listen: false).locale.languageCode,);
+    await Provider.of<BannerProvider>(context, listen: false)
+        .getBannerList(context, reload);
+    await Provider.of<ProductProvider>(context, listen: false).getDailyItemList(
+      context,
+      reload,
+      Provider.of<LocalizationProvider>(context, listen: false)
+          .locale
+          .languageCode,
+    );
     // await Provider.of<ProductProvider>(context, listen: false).getPopularProductList(context, '1', true);
     Provider.of<ProductProvider>(context, listen: false).getPopularProductList(
-      context, '1', reload, Provider.of<LocalizationProvider>(context, listen: false).locale.languageCode,
+      context,
+      '1',
+      reload,
+      Provider.of<LocalizationProvider>(context, listen: false)
+          .locale
+          .languageCode,
     );
   }
 
@@ -49,33 +64,51 @@ class HomeScreen extends StatelessWidget {
           child: SingleChildScrollView(
             controller: _scrollController,
             child: Center(
-              child: SizedBox(width: 1170,
+              child: SizedBox(
+                width: 1170,
                 child: Column(
-                   // controller: _scrollController,
+                    // controller: _scrollController,
                     children: [
+                      Consumer<BannerProvider>(
+                          builder: (context, banner, child) {
+                        return banner.bannerList == null
+                            ? BannersView()
+                            : banner.bannerList.length == 0
+                                ? SizedBox()
+                                : BannersView();
+                      }),
 
-                  Consumer<BannerProvider>(builder: (context, banner, child) {
-                    return banner.bannerList == null ? BannersView() : banner.bannerList.length == 0 ? SizedBox() : BannersView();
-                  }),
+                      // Category
+                      Consumer<CategoryProvider>(
+                          builder: (context, category, child) {
+                        return category.categoryList == null
+                            ? CategoryView()
+                            : category.categoryList.length == 0
+                                ? SizedBox()
+                                : CategoryView();
+                      }),
 
-                  // Category
-                  Consumer<CategoryProvider>(builder: (context, category, child) {
-                    return category.categoryList == null ? CategoryView() : category.categoryList.length == 0 ? SizedBox() : CategoryView();
-                  }),
+                      // Category
+                      Consumer<ProductProvider>(
+                          builder: (context, product, child) {
+                        return product.dailyItemList == null
+                            ? DailyItemView()
+                            : product.dailyItemList.length == 0
+                                ? SizedBox()
+                                : DailyItemView();
+                      }),
+                      // AD CARD
 
-                 // Category
-                  Consumer<ProductProvider>(builder: (context, product, child) {
-                    return product.dailyItemList == null ? DailyItemView() : product.dailyItemList.length == 0 ? SizedBox() : DailyItemView();
-                  }),
-
-                  // Popular Item
-                  Padding(
-                    padding: EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL),
-                    child: TitleWidget(title: getTranslated('popular_item', context)),
-                  ),
-                  ProductView(productType: ProductType.POPULAR_PRODUCT, scrollController: _scrollController),
-
-                ]),
+                      // Popular Item
+                      Padding(
+                        padding: EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL),
+                        child: TitleWidget(
+                            title: getTranslated('popular_item', context)),
+                      ),
+                      ProductView(
+                          productType: ProductType.POPULAR_PRODUCT,
+                          scrollController: _scrollController),
+                    ]),
               ),
             ),
           ),
